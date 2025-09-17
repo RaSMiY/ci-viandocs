14b13e4294b2615b3879cf6a086f2b23207ea5c7
Maksim Yu.Semenov <rasmiy@razum.top>
2025-09-17 17:32:46 +0200

    Добавлены заглавие и подножие страницы

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'ci-header',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HeaderComponent {
+  protected zoom = 100;
+}
